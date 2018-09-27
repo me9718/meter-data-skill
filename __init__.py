991dc2bd3a7e54d62b2ff2fa78b9d16e5e74f13d
me9718 <43108952+me9718@users.noreply.github.com>
2018-09-27 09:26:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,48 +1,64 @@
-from mycroft import MycroftSkill, intent_file_handler
-#from mycroft import intent_handler
-#from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill
-from mycroft.util.log import getLogger
 import socket
+from threading import Thread
+from mycroft import MycroftSkill, intent_file_handler
 
-LOGGER = getLogger(__name__)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-	s.bind(("192.168.1.103",5564))
-except socket.error as msg:
-	print("error occured")
-
-print("socket created")
-s.listen(10)
-
-while True:
-    conn, addr = s.accept()
-    print("client connected")
-
-    data = conn.recv(1024)
-    print("data received from client: ", data.decode('utf-8'))
+class MeterDataSkill(MycroftSkill):
+    def __init__(self):
+        super(MeterDataSkill, self).__init__(name="MeterDataSkill")
+        self.clientmsg = ""
+        self.is_shutdown = False
+        # Start a thread running the server accepting connections
+        self.thread = Thread(target=self.main_server)
+        self.thread.start()
     
-    if data is not None:
+    def main_server():
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        class MeterDataSkill(MycroftSkill):
-            def __init__(self):
-                super(MeterDataSkill, self).__init__(name="MeterDataSkill")
-    
-            result = data.decode('utf-8')
-            @intent_file_handler('data.meter.intent')
-            def handle_data_meter(self, message):
-                meter_type = message.data.get('type')
-                meter_value = self.result
-                print ("value: ", self.socket_server())
-                self.speak_dialog('data.meter', data={'type':meter_type, 'value':meter_value})
-    
-    #def stop(self):
-        #pass
-        #return False        
+        try:
+            # Listen on localhost, port 7724
+            s.bind(("127.0.0.1", 7724))
 
-    else:break
-    def create_skill():
-        return MeterDataSkill()
-conn.close()
-s.close()
+        except socket.error as msg:
+            self.log.info("error occured")
+            return False
+
+        # This code is not threaded
+        # and can ONLY handle one connected
+        # client at a time
+        s.listen(1)
+
+        self.log.info("socket created")
+
+        # Loop forever
+        while not self.is_shutdown:
+            # A new client connected
+            conn, addr = s.accept()
+            self.log.info("client connected", conn)
+
+            # Receive at most 1024 bytes and store in the class.
+            self.clientmsg = conn.recv(1024) 
+            self.log.info("data received from client: ", clientmsg)
+
+            # Echo back the received data to the client
+            conn.send(clientmsg)
+            self.log.info("clientmsg echoed back")
+
+            # Close connection to the client
+            conn.close()
+
+        s.close()
+        #return self.clientmsg
+    
+    @intent_file_handler('data.meter.intent')
+    def handle_data_meter(self, message):
+        meter_type = message.data.get('type')
+        #y = self.main_server
+        #meter_value = y
+        # Get last data received by server
+        meter_value = self.clientmsg
+        self.speak_dialog('data.meter', data={'type':meter_type, 'value':meter_value})
+    
+    
+def create_skill():
+    return MeterDataSkill()
